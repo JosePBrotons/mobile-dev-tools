81ef620c16b5c7ccda53b5fa84434fb7e8445542
Jose P. Brotons R <jose.brotons@almundo.com>
2020-12-05 18:29:08 -0500

    Changed Java's script path into a folder.

--- Java/java-dev-tools.sh
+++ Java/java-dev-tools.sh
@@ -1,3 +1,4 @@
+echo "Welcome, this script will install the dependencies and libraries that will help you throughout Java development"
 echo "Installing Java Development Kit (JDK)..."
 brew update && brew tap AdoptOpenJDK/openjdk && brew cask install adoptopenjdk11
 echo "Installing intelliJ IDEA Community Edition..."
